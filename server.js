fe6524c9dd772a14a245b6ec59370244e25bb0e6
Maryam <maryamnajibi55@gmail.com>
2022-04-14 23:38:00 -0400

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/maryam-najibi-a05'));
+app.use(express.static(__dirname + 'A04'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/maryam-najibi-a05/index.html'));
+res.sendFile(path.join(__dirname+'/dist/A04/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
